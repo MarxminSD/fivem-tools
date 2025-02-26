2a593d0f8b393f6db60ee1be4d5b75f2baff2a76
Marxmin <96161173+MarxminSD@users.noreply.github.com>
2025-02-25 23:11:54 -0500

    init

---
+++ src/app/inventory-tool/page.tsx
@@ -0,0 +1,7 @@
+export default function InventoryTool() {
+  return (
+    <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <h1 className="text-center text-4xl font-bold">Inventory Tool</h1>
+    </div>
+  );
+}
