8b1ab802d47027c20eb587bc46147466965f7352
Marxmin <96161173+MarxminSD@users.noreply.github.com>
2025-03-03 00:23:32 -0500

    feat: shadcn components added

---
+++ src/components/ui/Sonner.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Toaster as Sonner } from "sonner";
+
+type ToasterProps = React.ComponentProps<typeof Sonner>;
+
+const Toaster = ({ ...props }: ToasterProps) => {
+	const { theme = "system" } = useTheme();
+
+	return (
+		<Sonner
+			theme={theme as ToasterProps["theme"]}
+			className="toaster group"
+			duration={4500}
+			toastOptions={{
+				classNames: {
+					toast:
+						"group group-[.toaster]:p-2 select-none toast group-[.toaster]:bg-zinc-800 group-[.toaster]:text-zinc-100 group-[.toaster]:border-zinc-700 group-[.toaster]:shadow-lg",
+					description: "group-[.toast]:text-muted-foreground",
+					actionButton: "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+					cancelButton: "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+				},
+			}}
+			{...props}
+		/>
+	);
+};
+
+export { Toaster };
